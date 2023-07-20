61202227ab99bdccdcc4dc0eadf77b94345ccbcd
Chris Russell <crussel@estudiantec.cr>
2023-07-19 20:48:15 -0600

    Axbench initial commit

---
+++ axbench/applications/config.mk
@@ -0,0 +1,7 @@
+BASE_DIR    := /home/bradley/Projects/axbench
+FANN_INC	:= /usr/local/include
+FANN_LIB	:= /usr/local/lib
+PARROT_LIB  := $(BASE_DIR)/parrot.c/src/ParrotLib
+PLANG 		:= $(BASE_DIR)/parrot.c/src/ParrotObserver/plang.py
+PARROT_JSON := $(BASE_DIR)/parrot.c/src/ParrotObserver/ParrotC.json
+
